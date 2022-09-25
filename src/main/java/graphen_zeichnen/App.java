9776d755f247db7d999fab05e4dfc79ef3bc1af5
Farbkecks <fabian.alich@gmx.de>
2022-09-25 13:13:38 +0200

    first commit

---
+++ src/main/java/graphen_zeichnen/App.java
@@ -0,0 +1,13 @@
+package graphen_zeichnen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
