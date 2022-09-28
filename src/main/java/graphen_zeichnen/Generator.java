fbac1cc13d21c1457d71dedddf8273f28c8d41ae
“Farbkecks” <fabian.alich@gmx.de>
2022-09-28 11:20:03 +0200

    add splitFormulaInToArrayList

--- src/main/java/graphen_zeichnen/Generator.java
+++ src/main/java/graphen_zeichnen/Generator.java
@@ -5,13 +5,22 @@ import java.util.Arrays;
 
 public class Generator {
 
-    private static ArrayList<String> splitFormula(String formula) {
-        return new ArrayList<String>(Arrays.asList(formula.split("+-")));
+    private static ArrayList<String> splitFormulaInToArrayList(String formula) {
+        var splittedFormula = new ArrayList<String>();
+        var formulaChar = formula.toCharArray();
+        var start = 0;
+        for (int i = 0; i < formulaChar.length; i++) {
+            if (formulaChar[i] == '+' || formulaChar[i] == '-' && i != 0) {
+                splittedFormula.add(formula.substring(start, i));
+                start = i;
+            }
+        }
 
+        splittedFormula.add(formula.substring(start, formulaChar.length));
+        return splittedFormula;
     }
 
     public static void generateBasicGraph(String formula) {
-        var formulaList = splitFormula(formula);
-        System.out.println("lala");
+        var formulaList = splitFormulaInToArrayList(formula);
     }
 }
